b671ae1b3e5a04d5368028ff2e28a0456fe0e0b6
te9yie <te9yie@users.noreply.github.com>
2020-05-31 15:29:55 +0900

    first commit

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Tilemap map;
+    [SerializeField] private Tile wall;
+    [SerializeField] private Tile pass;
+
+    void Start()
+    {
+        var pos = new Vector3Int(0, 0, 0);
+        for (int y = -5; y < 5; ++y)
+        {
+            for (int x = -5; x < 5; ++x)
+            {
+                pos.x = x;
+                pos.y = y;
+                map.SetTile(pos, wall);
+            }
+        }
+    }
+
+    void Update()
+    {
+    }
+}
