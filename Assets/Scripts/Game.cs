8a9abf6c8adf6b3ca2d4bd3b139a8eee3ce0b978
te9yie <te9yie@users.noreply.github.com>
2020-05-31 16:52:03 +0900

    add player

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,27 +1,75 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 using UnityEngine.Tilemaps;
 
 public class Game : MonoBehaviour
 {
-    [SerializeField] private Tilemap map;
-    [SerializeField] private Tile wall;
-    [SerializeField] private Tile pass;
+    [SerializeField] private Tilemap map = default;
+    [SerializeField] private Tile wall = default;
+    [SerializeField] private Tile pass = default;
+    [SerializeField] private GameObject player = default;
+
+    private Vector3Int playerPos = new Vector3Int();
 
     void Start()
     {
-        var pos = new Vector3Int(0, 0, 0);
-        for (int y = -5; y < 5; ++y)
+        Assert.IsNotNull(map);
+        Assert.IsNotNull(wall);
+        Assert.IsNotNull(pass);
+        Assert.IsNotNull(player);
         {
-            for (int x = -5; x < 5; ++x)
+            var minX = -3;
+            var maxX = 3;
+            var minY = -3;
+            var maxY = 3;
+            var pos = new Vector3Int(0, 0, 0);
+            for (int y = minY; y <= maxY; ++y)
             {
-                pos.x = x;
-                pos.y = y;
-                map.SetTile(pos, wall);
+                for (int x = minX; x <= maxX; ++x)
+                {
+                    pos.x = x;
+                    pos.y = y;
+                    var tile = (y == minY || y == maxY || x == minX || x == maxX) ? wall : pass;
+                    map.SetTile(pos, tile);
+                }
             }
+            pos.x = -1;
+            pos.y = -1;
+            map.SetTile(pos, wall);
+        }
+        {
+            var loc = map.GetCellCenterLocal(playerPos);
+            player.transform.position = loc;
         }
     }
 
     void Update()
     {
+        var nextPos = playerPos;
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            --nextPos.x;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            ++nextPos.x;
+        }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            --nextPos.y;
+        }
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            ++nextPos.y;
+        }
+        if (nextPos != playerPos) {
+            var tile = map.GetTile(nextPos);
+            if (tile != null && tile == pass)
+            {
+                playerPos = nextPos;
+                var loc = map.GetCellCenterLocal(playerPos);
+                player.transform.position = loc;
+            }
+        }
     }
 }
